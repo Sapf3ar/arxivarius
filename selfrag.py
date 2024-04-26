b6ebf07af0d84a06e6896942dde7aa98beecc199
Daniel <sapfaer@Daniels-MacBook-Pro.local>
2024-04-26 09:29:18 +0300

    clean

--- selfrag.py
+++ selfrag.py
@@ -1,26 +1,15 @@
-import operator
-from prompts import research_prompt
+from code_writer import CodeWrite
+from prompts import get_research_prompt, lector_prompt
 from langchain_core.prompts import ChatPromptTemplate
 import streamlit as st
 import os
 import logging
-import json
-from typing import Annotated, Sequence, TypedDict
 from langchain.chat_models.gigachat import GigaChat
-from langchain_core.tools import tool
-from langchain import hub
-from langchain.output_parsers import PydanticOutputParser
-from langchain.prompts import PromptTemplate
-from langchain_core.utils.function_calling import convert_to_gigachat_tool, convert_to_gigachat_function
 from langchain_core.messages import BaseMessage, FunctionMessage, HumanMessage, SystemMessage, AIMessage
-from langchain.output_parsers.openai_tools import PydanticToolsParser
-from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.output_parsers import StrOutputParser
-from langchain.tools.retriever import create_retriever_tool
 from langchain_community.vectorstores.faiss import FAISS
 
 from embedder import FastEmbeddings, download_papers, embed_docs
-from parser import get_correct_name_from_topk
 
 
 
@@ -41,27 +30,14 @@ def paper_search(query:Annotated[str, "Запрос для поиска внут
     return answer
 
 
-@tool
-def ref_search( link:Annotated[str, "Название статьи из списка предоставленных."],
-               query:Annotated[str, "Запрос для поиска внутри загруженной статьи."]):
-    '''
-    Используй этот инструмент для того чтобы создать поисковый индекс из предоставленных тебе статей
-    и получить нужную тебе информацию из связанных статей. Принимай взвешенное решение об использовании этого 
-    инструмента на основе данных тебе названий связанных статей.
-    '''
-    references = st.session_state.refs
-    name_ref = get_correct_name_from_topk(titles=[i['misc'] for i in references], 
-                                label=link)
-    
-    download_id = references[name_ref]['id']
 
-    doc = download_papers([download_id])
-    return answer
 
 class WorkFlow():
     def __init__(self):
-        self.tools = [ref_search, paper_search]
-       
+        self.coder = CodeWrite("code_prompt.txt") 
+        self.base_db = FAISS.load_local("fais_db/", 
+                                        embeddings=FastEmbeddings(),
+                                        allow_dangerous_deserialization=True)
     def add_current_paper_rag(self, db) -> None:
         self.curr_db = db
 
@@ -93,12 +69,13 @@ class WorkFlow():
             ]
         }
         outputs = self.rag_model.invoke(inputs['messages'])                
-        response = self.call_rag_paper(query = outputs.content[0].split('"')[-1])
+        response = self.call_rag_paper(query = outputs.content[0].split('"')[-1],
+                                       db=self.curr_db)
         return response
    
-    def call_rag_paper(self, query):
+    def call_rag_paper(self, query, db):
         paper_info = paper_search(query, 
-                                  db=self.curr_db)
+                                  db=db)
         
         rag_output = {"context":paper_info, 
                       "question": "Суммаризуй предоставленный тебе контекст и переведи его на русский язык, не теряя наученые термины и нюансы описания алгоритма."}
@@ -117,66 +94,79 @@ class WorkFlow():
         response = rag_chain.invoke(rag_output)
         return response
 
-
+    def rewrite(self, input):
+        pass
     def call_researcher(self, human_input, rag_response):
         model = GigaChat(
                     credentials=os.environ['GIGA_TOKEN'],
                     verify_ssl_certs=False,
-                    model="GigaChat-Pro-preview",
+                    model="GigaChat-Pro",
                     scope='GIGACHAT_API_CORP',
                     temperature=0.6,
                     top_p=0.4)
+        
         input_prompt = {"name":st.session_state.name, 
-                        "context":rag_response} 
-        res_chain = research_prompt | model
+                        "context":rag_response,
+                        "user_query":human_input,
+                        "query1":" ",
+                        "query2":" ",
+                        "code1": " "} 
+        res_chain = get_research_prompt() | model | StrOutputParser()
         response = res_chain.invoke(input_prompt) 
+        for rec in range(3):
+            if "END" in response:
+                break
+            if "Запрос1" in response:
+                output = self.call_rag_paper(response.split("Запрос1:")[-1],
+                                             db=self.curr_db)
+                input_prompt['query1'] = output
+            if "Код1" in response:
+                output = self.call_engineer(response.split("Код1:")[-1])
+                input_prompt['code1'] = output
+            if "Запрос2" in response:
+                output = self.call_rag_paper(response.split("Запрос2:")[-1],
+                                             db=self.base_db)
+                input_prompt['query2'] = output
 
-        if "Запрос1" in response.content:
-            output = self.call_rag_paper(response.content.split("Запрос1:")[-1])
-            logging.warning(f"first query {output}")
 
-        return response
+            logging.warning(input_prompt)        
+            response = res_chain.invoke(input_prompt)
 
-    def call_engineer(self):
-        return
+        return response, get_research_prompt().invoke(input_prompt)
+
+    def call_engineer(self, query):
+        code = self.coder.code_generator(query)
+        logging.warning(code)
+        return code
 
     def execute_steps(self, human_input:str):
         base_paper_data = self.entry(human_input=human_input)
-        research_plan = self.call_researcher(human_input=human_input,
+        response, prompt = self.call_researcher(human_input=human_input,
                                              rag_response=base_paper_data)
-
-        return research_plan
-
-    def build_agents(self, ):
+        lector_output = self.call_lector(input=human_input,
+                                         response=prompt)
         
-        self.res_llm = GigaChat(
-            credentials=os.environ["GIGA_TOKEN"],
-            verify_ssl_certs=False,
-            model="GigaChat-Pro",
-            scope='GIGACHAT_API_CORP',
-            temperature=0.5,
-            top_p=0.5
+        logging.warning(f"LECTOR OUTPUT {lector_output}")
+        return lector_output 
 
-        )
+    def call_lector(self, input, response):
+        model = GigaChat(credentials=os.environ['GIGA_TOKEN'],
+                        verify_ssl_certs=False,
+                        model="GigaChat-Pro",
+                        scope='GIGACHAT_API_CORP',
+                        temperature=0.3,
+                        top_p=0.5)
+        prompt = lector_prompt()
+        inputs = {"user_input":input,
+                  "query":response}
+        lector_chain = prompt | model | StrOutputParser()
+        lector_perm = lector_chain.invoke(inputs)
+        return lector_perm
 
-
-        self.engineer_llm = GigaChat(
-            credentials=os.environ["GIGA_TOKEN"],
-            verify_ssl_certs=False,
-            model="GigaChat-Pro",
-            scope='GIGACHAT_API_CORP',
-            temperature=0.3,
-            top_p=0.2)
-
-         
-
-        self.lector_llm = GigaChat(
-            credentials=os.environ["GIGA_TOKEN"],
-            verify_ssl_certs=False,
-            model="GigaChat-Pro",
-            scope='GIGACHAT_API_CORP',
-            temperature=0.5,)
- 
+    def build_response(self, response, result):
+        if result:
+            response = ""
+  
 
 
 
